5a18f431d9f3c25c466fdfefd786ac85e0acef84
ayush-uk <ayushukenday@gmail.com>
2026-03-26 20:00:14 +0530

    interview integration

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import connectDb from './config/connectDb.js';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
+import interviewRouter from './routes/interview.route.js';
 
 
 dotenv.config();
@@ -26,7 +27,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
-
+app.use("/api/interview" , interviewRouter)
 
 
 app.listen(PORT, () => {
